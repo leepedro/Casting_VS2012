f4a3af9eaaad86e0a0d2f4bf527bc4e5dfd55fd2
Cheolha Pedro Lee <leepedro@gmail.com>
2013-08-01 04:14:23 -0400

    checked out numeric_limits<floating>::min()

--- DataRange/test_data_range.cpp
+++ DataRange/test_data_range.cpp
@@ -1,24 +1,23 @@
 #include <limits>
 #include <iostream>
 
-template <typename T, typename U>
-typename std::enable_if<std::is_integral<T>::value && std::is_floating_point<U>::value, void>::type integer_vs_floating(void)
-{
-	int i_min(std::numeric_limits<T>::min()), i_max(std::numeric_limits<I>::max());
-	float f_min(std::numeric_limits<U>::min()), f_max(std::numeric_limits<U>::max());
+/* integer to floating, floating to integer
 
-	std::cout << typeid(T).name() << ": " << i_min << " ~ " << i_max << std::endl;
-	std::cout << typeid(U).name() << ": " << f_min << " ~ " << f_max << std::endl;
+floating to integer casting shows some unexpected behaviors by setting the value as the
+negative minimum value.
+integer to floating casting seems working and the dynamic range of floating point data type
+is practically wider than its integer counter part with the same memory size.
 
-	if (static_cast<float>(i_min) < f_min)
-		std::cout << "float(i_min) < f_min" << std::endl;
-	if (i_min < static_cast<int>(f_min))
-		std::cout << "i_min < int(f_min)" << std::endl;
-	if (static_cast<float>(i_max) < f_max)
-		std::cout << "float(i_max) < f_max" << std::endl;
-	if (i_max <= static_cast<int>(f_max))	// NO!!! -> Do NOT cast float to int!
-		std::cout << "i_max <= int(f_max)" << std::endl;
-}
+-> Use static_cast<T> without any value checking for integer to floating point casting.
+-> Must check the value for floating point to integer casting.
+
+@Example
+if (static_cast<floating>(integer_max) < f) || (static_cast<floating>(integer_min) > f)
+ throw std:overflow();
+else
+ return static_cast<integer>(f);
+
+ */
 
 void test1(void)
 {
@@ -34,24 +33,82 @@ void test1(void)
 
 	float float_i_min(static_cast<float>(i_min));
 	if (float_i_min > f_max_neg)
-		std::cout << "float(i_min) > f_min" << std::endl;
-
-	if (static_cast<float>(i_min) < f_min)	// YES
+		std::cout << "float(i_min) > f_min" << std::endl;	// float is practically wider than int
+	else
 		std::cout << "float(i_min) < f_min" << std::endl;
-	if (i_min < static_cast<int>(f_min))	// YES
-		std::cout << "i_min < int(f_min)" << std::endl;
-	if (static_cast<float>(i_max) < f_max)	// YES
-		std::cout << "float(i_max) < f_max" << std::endl;
-	if (i_max <= static_cast<int>(f_max))	// NO!!! -> Do NOT cast float to int!
-		std::cout << "i_max <= int(f_max)" << std::endl;
-	int int_f_max = static_cast<int>(f_max);	// Overflowed to a negative value!!!
 
-	int int_float_i_max = static_cast<int>(static_cast<float>(i_max) + 1.0f);	// Overflowed to a negative value!!!
+	int int_f_max_neg(static_cast<int>(f_max_neg));
+	if (i_min < int_f_max_neg)
+		std::cout << "i_min < int(f_min)" << std::endl;	
+	else if (i_min == int_f_max_neg)
+		std::cout << "i_min == int(f_min)" << std::endl;	// saturated to min value instead of overflow
+	else
+		std::cout << "i_min > int(f_min)" << std::endl;
+
+	float float_i_max(static_cast<float>(i_max));
+	if (float_i_max < f_max)
+		std::cout << "float(i_max) < f_max" << std::endl;	// float is practically wider than int
+	else
+		std::cout << "float(i_max) > f_max" << std::endl;
+
+	int int_f_max(static_cast<int>(f_max));
+	if (i_max > int_f_max)
+		std::cout << "i_max > int(f_max)" << std::endl;		// Overflowed and saturated to a negative min value!!!
+	else if (i_max == int_f_max)
+		std::cout << "i_max == int(f_max)" << std::endl;
+	else
+		std::cout << "i_max < int(f_max)" << std::endl;	
+
+	int int_float_i_max = static_cast<int>(static_cast<float>(i_max) + 1.0f);	// Overflowed and saturated to a negative min value!!!
+}
+
+void test2(void)
+{
+	long long l_min(std::numeric_limits<long long>::min()), l_max(std::numeric_limits<long long>::max());
+	double d_min(std::numeric_limits<double>::min()), d_max(std::numeric_limits<double>::max());
+	double d_max_neg = -d_max;
+
+	if (d_min > 0)
+		std::cout << "d_min > 0 !!!" << std::endl;	// !!!! The minimum double value is POSITIVE!!!
+
+	std::cout << "long long: " << l_min << " ~ " << l_max << std::endl;
+	std::cout << "double: " << d_max_neg << " ~ " << d_min << " ~ " << d_max << std::endl;
+
+	double double_l_min(static_cast<double>(l_min));
+	if (double_l_min > d_max_neg)
+		std::cout << "double(l_min) > d_min" << std::endl;	// double is practically wider than long long
+	else
+		std::cout << "double(l_min) < d_min" << std::endl;
+
+	long long long_d_max_neg(static_cast<long long>(d_max_neg));
+	if (l_min < long_d_max_neg)
+		std::cout << "l_min < long long(d_min)" << std::endl;	
+	else if (l_min == long_d_max_neg)
+		std::cout << "l_min == long long(d_min)" << std::endl;	// saturated to min value instead of overflow
+	else
+		std::cout << "l_min > long long(d_min)" << std::endl;
+
+	double double_l_max(static_cast<double>(l_max));
+	if (double_l_max < d_max)
+		std::cout << "double(l_max) < d_max" << std::endl;	// double is practically wider than long long
+	else
+		std::cout << "double(l_max) > d_max" << std::endl;
+
+	long long long_d_max(static_cast<long long>(d_max));
+	if (l_max > long_d_max)
+		std::cout << "l_max > long long(d_max)" << std::endl;		// Overflowed and saturated to a negative min value!!!
+	else if (l_max == long_d_max)
+		std::cout << "l_max == long long(d_max)" << std::endl;
+	else
+		std::cout << "l_max < long long(d_max)" << std::endl;	
+
+	long long long_double_l_max = static_cast<long long>(static_cast<double>(l_max) + 1.0f);	// Overflowed and saturated to a negative min value!!!
 }
 
 int main(void)
 {
 	test1();
+	test2();
 	//int ll_min(std::numeric_limits<long long>::min()), ll_max(std::numeric_limits<long long>::max());
 	//float f_min(std::numeric_limits<float>::min()), f_max(std::numeric_limits<float>::max());
 
